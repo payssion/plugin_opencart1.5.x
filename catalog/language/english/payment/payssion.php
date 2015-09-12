c15e414e96e3e75d7d6be715678e8a95119d232f
Dong <dong.liu@payssion.com>
2015-09-12 15:44:40 +0800

    added more payment options

--- catalog/language/english/payment/payssion.php
+++ catalog/language/english/payment/payssion.php
@@ -16,6 +16,25 @@ $_['text_title_webmoneyjp']		    = 'WebMoney Japan';
 $_['text_title_openbucks']		    = 'Pay with Gift Cards using Openbucks';
 $_['text_title_trustpay']		    = 'European bank transfer';
 
+$_['text_title_fpxmy']		    = 'MyClear FPX';
+$_['text_title_maybank2umy']		= 'Maybank2u';
+$_['text_title_cimbmy']		    = 'CIMB Clicks';
+$_['text_title_rhbmy']		    = 'RHB Now';
+$_['text_title_ambmy']		    = 'Am Online';
+$_['text_title_hlbmy']		    = 'Hong Leong Connect';
+$_['text_title_affinepgmy']		= 'Affin Bank';
+$_['text_title_webcashmy']		= 'Webcash';
+$_['text_title_7elevenmy']		= '7­eleven Malaysia';
+$_['text_title_epaymy']		    = 'epay';
+$_['text_title_esapaymy']		= 'EsaPAY';
+$_['text_title_enetssg']		    = 'eNETS Debit';
+$_['text_title_singpostsg']	    = 'SAM by SingPost';
+$_['text_title_dragonpayph']		= 'Dragonpay';
+$_['text_title_nganluongvn']		= 'Nganluong';
+$_['text_title_atmvaid']		    = 'Indonesia ATM Network Transfer Virtual Account';
+$_['text_title_paysbuyth']		= 'Paysbuy';
+$_['text_title_molpay']		= 'MOLPay';
+
 $_['text_response']				= '';
 $_['text_success']				= '... your payment was successfully received.';
 $_['text_success_wait']			= '<b><span style="color: #FF0000">Please wait...</span></b> while we finish processing your order.<br>If you are not automatically re-directed in 10 seconds, please click <a href="%s">here</a>.';
